2e647a05c30dc77eae3942952fe027fd3996734a
JeremyWelsh <welshjm15@gmail.com>
2021-08-16 17:41:56 +1200

    Created New screens, not in navigation yet

---
+++ app/screens/BuddyScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, View } from "react-native";
+
+import colours from '../config/colours';
+
+const BuddyScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Popcorn Buddies BuddyScreen</Text>
+            <StatusBar style="auto" />
+        </View>
+
+    );
+  } 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colours.primary,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+})
+
+
+
+export default BuddyScreen;
