0a1be880443875feef68105c5b54338cc3f2c34f
Jeremy Welsh <welshjere@ecs.vuw.ac.nz>
2021-09-24 21:37:09 +1200

    have a list of all users working

--- app/screens/BuddyScreen.js
+++ app/screens/BuddyScreen.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from "react-native";
-
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import {auth, db} from '../../firebase'
+import { ActivityIndicator } from 'react-native';
 import colours from '../config/colours';
+import { Button } from 'react-native-elements/dist/buttons/Button';
 
 // screen where a user sees all of their buddies
 const BuddyScreen = ({navigation}) => {
+    const [loading, setLoading] = useState(true); 
+    const [buddies, setBuddies] = useState([]); 
+
+    /*
+    const getBuddies = async () => {
+        const usersRef = db.collection('users');
+        const snapshot = await usersRef.get();
+        if (snapshot.empty) {
+            console.log('No matching documents.');
+            return;
+        }  
+
+        snapshot.forEach(doc => {   
+            console.log(doc.data().buddyName, '=>', doc.data().email);
+        });
+    }
+*/
+    useEffect(() => {
+        const subscriber = db.collection('users').onSnapshot(snapshot => {
+            const buddies = [];
+            snapshot.forEach(doc => {
+              buddies.push({
+                ...doc.data(),
+                key: doc.id,
+              });
+            });
+      
+            setBuddies(buddies);
+            setLoading(false);
+          });
+        return () => subscriber();
+      }, []);
+
+
+
+
+
+
+    if (loading) {
+        return <ActivityIndicator />;
+    }else{
+
     return (
         <View style={styles.container}>
             <Text>Popcorn Buddies BuddyScreen</Text>
             <StatusBar style="auto" />
+            <FlatList
+                data={buddies}
+                renderItem={({ item }) => (
+            <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>User ID: {item.id}</Text>
+            <Text>User Name: {item.buddyName}</Text>
+        </View>
+      )}
+    />
+            <Button containerStyle={styles.button} title="Add Buddy" onPress={()=>{console.log("lol")}} />
         </View>
 
-    );
-  } 
+        );
+    } 
+    }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
