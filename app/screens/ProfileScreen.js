acf3646ede7e2196659336c975c323a368c8be92
JeremyWelsh <welshjm15@gmail.com>
2021-09-25 11:43:53 +1200

    got most of formatting sorted

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -3,15 +3,18 @@ import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import {auth, db} from '../../firebase'
 import { ActivityIndicator } from 'react-native';
+import { Image } from 'react-native-elements/dist/image/Image';
 import colours from '../config/colours';
 import { Button } from 'react-native-elements/dist/buttons/Button';
+import { Rating } from 'react-native-elements';
 
+
+const Image_Link = "https://image.tmdb.org/t/p/w200";
 // user viewing their own profile
 const ProfileScreen = ({navigation}) => {
     
     const [isLoading, setLoading] = useState(true); 
     const [reviews, setReviews] = useState([]); 
-
 
     const LogOut = () => {
         auth.signOut().then(()=>{
@@ -19,7 +22,29 @@ const ProfileScreen = ({navigation}) => {
         })
     }
 
-    /*
+    function compareRating( a, b ) {
+        if ( a.rating > b.rating ){return -1;}
+        if ( a.rating < b.rating ){return 1;}
+        return 0;
+      }
+      
+    useEffect(() => {
+        const subscriber = db.collection('users').doc(auth.currentUser.uid).collection('reviews').onSnapshot(snapshot => {
+            const reviews = [];
+            snapshot.forEach(doc => {
+              reviews.push({
+                ...doc.data(),
+                key: doc.id,
+              });
+            });
+            reviews.sort(compareRating);
+            setReviews(reviews);
+            setLoading(false);
+          });
+        return () => subscriber();
+      }, []);
+
+      /*
     .add({
           cID: contentId,
           cName: contentName,
@@ -30,28 +55,25 @@ const ProfileScreen = ({navigation}) => {
           poster: ppath
         })
         */
-    useEffect(() => {
-        const subscriber = db.collection('users').doc(auth.currentUser.uid).collection('reviews').onSnapshot(snapshot => {
-            const reviews = [];
-            snapshot.forEach(doc => {
-              reviews.push({
-                ...doc.data(),
-                key: doc.id,
-              });
-            });
-            setReviews(reviews);
-            setLoading(false);
-            console.log(reviews)
-          });
-        return () => subscriber();
-      }, []);
 
       const renderItem = ({ item }) => {
         return(
-          <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>User ID: {item.rating*2}</Text>
-          <Text>User Name: {item.cName}</Text>
-          </View>
+            <View style={[styles.item]}>
+                <View style={{alignItems:'flex-start', flex:1, paddingRight:15}}>
+                    <Text style={styles.title}>Title: {item.cName}</Text>
+                    <Rating imageSize={20}
+                        type= "custom"
+                        readonly 
+                        startingValue={item.rating/2}
+                        ratingColor="#fff"
+                        tintColor="#95D7AE"
+                        ratingBackgroundColor= "#000"/>
+                    <Text style={styles.extrainfo}>Year: {item.cYear}</Text>
+                    <Text style={styles.extrainfo}>Content Type: {item.type}</Text>
+                    <Text style={styles.extrainfo}>Comment: {item.comment}</Text>
+                </View>
+                <Image source={item.poster?{ uri: `${Image_Link+item.poster}`}:null} style={{width:125, height: 180,}}/>
+            </View>
         );
     };
 
@@ -80,8 +102,22 @@ const styles = StyleSheet.create({
         width: 350,
         marginTop: 5,
         backgroundColor: '#19323C',
+    },item: {
+        backgroundColor: '#19323C',
+        padding: 20,
+        marginVertical: 7,
+        marginHorizontal: 12,
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+      },
+      title: {
+        fontWeight: 'bold',
+        fontSize: 27,
 
-    },
+      },
+      extrainfo: {
+        fontSize: 15,
+      },
 })
 
 
